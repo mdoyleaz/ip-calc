5d4105d3b4da55da24fda579dcfbaf712a4ca837
Michael D <mdoyleazz@gmail.com>
2019-02-18 01:16:17 -0700

    Added some docker support for frontend with nginx

--- api/run.py
+++ api/run.py
@@ -2,7 +2,6 @@
 
 import os
 
-from flask import render_template
 from ipcalc import create_app
 
 config_name = os.getenv('FLASK_CONFIG')
@@ -10,4 +9,4 @@ app = create_app(config_name)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run()
---
+++ frontend/build/precache-manifest.a7c9c639393792962d1f0c368ebdaa4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "efe0bcedec9677f3b05d",
+    "url": "/static/js/main.efe0bced.chunk.js"
+  },
+  {
+    "revision": "cc8c893d1060355f8605",
+    "url": "/static/js/2.cc8c893d.chunk.js"
+  },
+  {
+    "revision": "efe0bcedec9677f3b05d",
+    "url": "/static/css/main.56f5d9ea.chunk.css"
+  },
+  {
+    "revision": "b4e697101993a93e278f9a5ee32b5c24",
+    "url": "/index.html"
+  }
+];
